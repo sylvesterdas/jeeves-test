c002f62d21943d1cdd6ef1d0bb3a0ff7b640622b
Sylvester Das <sylvester.das@scitara.com>
2021-11-02 18:22:02 +0530

    index and app

---
+++ src/account/index.js
@@ -0,0 +1,76 @@
+const { Router } = require("express");
+const { User } = require('../model/user');
+const { hash, compare } = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
+
+const accountRouter = new Router({ strict: true });
+
+accountRouter.post("/register", async (req, res) => {
+    try {
+        const { first_name, last_name, email, password } = req.body;
+
+        if (!(email && password && first_name && last_name)) {
+            return res.status(400).send("All input is required");
+        }
+
+        const oldUser = await User.findOne({ email });
+
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+
+        const encryptedPassword = await hash(password, 10);
+
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        const token = sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+        // save user token
+        user.token = token;
+
+        return res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).end();
+    }
+});
+
+accountRouter.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+    
+        if (!(email && password)) {
+          return res.status(400).send("All input is required");
+        }
+        const user = await User.findOne({ email });
+    
+        if (user && (await compare(password, user.password))) {
+          const token = sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+              expiresIn: "2h",
+            }
+          );
+    
+          user.token = token;
+    
+          return res.status(200).json(user);
+        }
+        return res.status(400).send("Invalid Credentials");
+      } catch (err) {
+        console.log(err);
+      }
+});
+
+module.exports = accountRouter;
---
+++ src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const { auth } = require('./middleware/auth');
+const accountRouter = require('./account');
+const topicRoutes = require('./topics');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use((req, _, next) => {
+    console.debug('Incoming', req.method, req.path);
+    next();
+})
+
+app.get('/', (_, res) => res.send({ status: 'online' }))
+
+app.use('/account', accountRouter);
+app.use('/topics', auth, topicRoutes);
+
+exports.start = async function start(PORT = process.env.PORT || 3000) {
+    await require('./config/database').connect();
+    app.listen(PORT, () => console.log('Listening on PORT', PORT));
+}
+
+exports.app = app;
