12cb7a33b4fff1b64c5bb158b01bbecb443e0e4e
Sylvester Das <sylvester.das@scitara.com>
2021-11-02 18:22:26 +0530

    Added auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const { verify } = require("jsonwebtoken");
+
+const { TOKEN_KEY } = process.env;
+
+const auth = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    const decoded = verify(token, TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
+
+exports.auth = auth;
---
+++ src/model/user.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+    first_name: { type: String, default: null },
+    last_name: { type: String, default: null },
+    email: { type: String, unique: true },
+    password: { type: String },
+    token: { type: String },
+}, {
+    collection: 'users',
+    timestamps: true,
+    toObject: {
+        transform: function (doc, ret) {
+            delete ret._id;
+        }
+    },
+    toJSON: {
+        transform: function (doc, ret) {
+            delete ret._id;
+        }
+    }
+}); 
+
+exports.User = model("user", userSchema);
